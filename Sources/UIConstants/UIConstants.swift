c1db09dc1a52ab892ec5ec93e24e040cfd9abe6b
yukonblue <77284557+yukonblue@users.noreply.github.com>
2022-11-30 11:16:21 -0500

    Port over source

--- Sources/UIConstants/UIConstants.swift
+++ Sources/UIConstants/UIConstants.swift
@@ -1,6 +1,22 @@
-public struct UIConstants {
-    public private(set) var text = "Hello, World!"
+//
+//  UIConstants.swift
+//  UIConstants
+//
+//  Created by yukonblue on 07/20/2022.
+//
 
-    public init() {
-    }
+import CoreGraphics
+
+public enum CornerRadius: CGFloat {
+
+    case mini = 6.0
+    case tiny = 12.0
+    case small = 20.0
+    case medium = 24.0
+    case large = 28.0
+}
+
+public enum ShadowRadius: CGFloat {
+
+    case toolbarControl = 5.0
 }
